a657f95c43fc7104b6c0efdb4aac0efb1f0667f7
Epic Verity <epicverity@hotmail.com>
2014-02-25 19:39:11 -0500

    Here I think it is!

---
+++ unity/Assets/Scripts/Cube2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		renderer.material.color = Color.blue;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity/Assets/Scripts/EventManger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManger : MonoBehaviour {
+	//public delegate void CollideHandler (bool isCollide);
+	//public static event CollideHandler onCollide;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision hit)
+	{
+		if (hit.gameObject.tag == "cube1")
+		{
+			GameObject.Find("Sphere").renderer.material.color = Color.red;
+		}
+		if (hit.gameObject.tag == "cube2")
+		{			
+			GameObject.Find("Sphere").renderer.material.color = Color.yellow;		
+		}
+	}
+}
